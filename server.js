0cc008545f9192e6bf0ac8e70ba5a229780a0427
Shell-thon <shellterminal0@gmail.com>
2022-12-27 17:17:59 +0000

    added form handling for html page in server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.end('Hello World!');
+	if (req.method === 'POST') {
+		let body = '';
+		req.on('data', chunk => {
+			body += chunk.toString();
+		});
+		req.on('end', () => {
+			const formData = querystring.parse(body);
+			res.end(`Your name is ${formData.name}`);
+		});
+	}
 });
 
-server.listen(3000, '0.0.0.0', () => {
-	console.log('Server running at 3000');
+server.listen(3000, () => {
+	console.log('Server listening on port 3000');
 });
